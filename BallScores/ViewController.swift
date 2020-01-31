cdb7cb27d9bf3557ae98d8c4bc4323d6973f3f3c
Gene <yeegenetan@gmail.com>
2020-01-31 22:17:45 +0800

    Initial Commit

---
+++ BallScores/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BallScores
+//
+//  Created by Tan Yee Gene on 31/01/2020.
+//  Copyright © 2020 Tan Yee Gene. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
